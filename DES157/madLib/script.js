24825627d230c5f310dd2f181a4b0813ddc4fb7e
Tong Su <tonsu@ucdavis.edu>
2021-02-09 10:21:49 -0800

    Mad_lib

--- DES157/madLib/script.js
+++ DES157/madLib/script.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
-    console.log("reading js")
 
+    //star function
     function starInit(starCount){
         const starArea = document.querySelector(".starArea");
         for (let i = 0; i < starCount; i++) {
@@ -24,65 +24,58 @@
         }
     }
     starPosition(200);
+    //main
+    const myform = document.querySelector('#myform');
+    const madlibcontent = document.querySelector('#madlibcontent');
+    const errorcontent = document.querySelector('#errorcontent');
 
-    const myForm = document.querySelector ('#myform'); 
-    const madlib = document.querySelector ('#madlib');
+    myform.addEventListener('submit', function(e){
+        e.preventDefault();
+        const noun1 = document.querySelector('#noun1').value;
+        const num1 = document.querySelector('#num1').value;
+        const noun2 = document.querySelector('#noun2').value;
+        const num2 = document.querySelector('#num2').value;
+        const noun3 = document.querySelector('#noun3').value;
+        const adj = document.querySelector('#adj').value;
 
-    
-    myForm.addEventListener('submit', function(event){
-        event.preventDefault(); 
-        const noun1 = document.querySelector('#noun1').value; 
-        const integer1 = document.querySelector('#integer1').value; 
-        const noun2 = document.querySelector('#noun2').value; 
-        const integer2 = document.querySelector('integer2').value; 
-        const noun3 = document.querySelector('#noun3').value; 
-        const adj2 = document.querySelector('#adj2').value; 
-        
-        let myText;
-     
+        let reText;
 
-        if (noun1 && integer1  && noun2 && integer2 && noun3  && adj2) { 
-            myText =  `You accidentally crossed the wormhole during
+        if (noun1 && num1 && noun2 && num2 && noun3 && adj) {
+            document.getElementById('overlay').className = "showing";
+            reText = `You accidentally crossed the wormhole during
             an interstellar journey and arrived at a planet
-            called ${noun1} Star ${integer1} light years away from
+            called ${noun1} Star ${num1} light years away from
             The Earth. This planet is very suitable for life 
             and its specialty is ${noun2}. Fortunately, there are 
-            ${integer2} ${noun3} from the earth on your spaceship. 
+            ${num2} ${noun3} from the earth on your spaceship. 
             You built a ${noun3} empire together, and you 
-            have lived a ${adj2} life ever since.`; 
+            have lived a ${adj} life ever since.`;
+
+            madlibcontent.innerHTML = reText;
         }
-        else { 
-            myText = "Please give me words so I can make your Mad Libl";
+        else {
+            document.getElementById('error').className = "showing";
+            reText = "Please fill out all the fields.";
+
+            errorcontent.innerHTML = reText;
         }
 
-        madlib.innerHTML = myText; 
+        const formData = document.querySelectorAll("input[type=text]");
 
-        var formData = document.querySelectorAl1("input[type=text]"); 
-        
-        for (var eachField of formData) { 
-            eachField.value = ""; 
+        for (const eachField of formData) {
+            eachField.value = "";
         }
-        
+    });
+    //close botton
+    document.querySelector('.close').addEventListener('click', function (e) {
+        e.preventDefault();
+        document.getElementById('overlay').className = "hidden";
     });
 
-
-      
-    /*document.querySelector('.open').addEventListener('click', function (event) { 
-        event.preventDefault(); 
-        document.getElementById('overlay').className = 'showing'; 
-    }); 
-    
-    document.querySelector('.close').addEventListener('click', function (event) { 
-        event.preventDefault(); 
-        document.getElementById('overlay').className = 'hidden';
-
+    document.addEventListener('keydown', function (e) {
+        if(e.key === 'Escape') {
+            document.getElementById('overlay').className = "hidden";
+        }
     });
 
-    document.addEventListener('keydown', function (event) { 
-        if (event.key == 'Escape') { 
-            document.getElementById('overlay').className = 'hidden';
-        }
-    });*/
-    
-
-})();
+}());
